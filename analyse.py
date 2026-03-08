e9d77672e3c76cbf41c751f6db4ffd202ea2ff85
xandasoneill <xandas.oneill@gmail.com>
2026-03-08 16:22:07 +0000

    new analyse.py to run analysis functions

---
+++ analyse.py
@@ -0,0 +1,3 @@
+from visualizations import load_and_plot_results, plot_accuracy_comparison, plot_variance_comparison, plot_learning_curves, plot_accuracy_evolution
+
+load_and_plot_results("DeltaGrad_results_batch16_lr0.03194028510565753.pkl","DeltaGrad")
