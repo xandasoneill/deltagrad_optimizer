62eed8d798d005f42cdad63fe614599a0cf7427f
xandasoneill <xandas.oneill@gmail.com>
2026-03-18 08:17:59 +0000

    optimization for bs 512 50 epochs done

--- analyse.py
+++ analyse.py
@@ -1,8 +1,8 @@
 from visualizations import load_and_plot_results, plot_accuracy_comparison, plot_variance_comparison, plot_learning_curves, plot_accuracy_evolution, plot_mean_time_per_epoch
 import joblib
 
-results_adam=joblib.load("Adam_results_batch64_lr0.0003425316690148133.pkl")
-results_dg =joblib.load("DeltaGrad_results_batch64_lr0.10088051249528875.pkl")
+results_adam=joblib.load("results/results_batchtest/results_values/Adam_results_batch16_lr0.0003425316690148133.pkl")
+results_dg =joblib.load("results/results_batchtest/results_values/DeltaGrad_results_batch16_lr0.10088051249528875.pkl")
 load_and_plot_results(results_dg, results_adam)
 print(results_adam["all_total_times"])
 print(results_dg["all_total_times"])
